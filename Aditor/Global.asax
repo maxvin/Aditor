4a1afa46207034e0a68859926819dde176e059ec
MaksymP <max_17@mail.ru>
2015-08-10 17:25:35 +0300

    Initial solution

---
+++ Aditor/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Aditor.MvcApplication" Language="C#" %>
---
+++ Aditor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Aditor.Startup))]
+namespace Aditor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
